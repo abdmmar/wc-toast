c807fcbd75f77b699ef863e0425d8948803151dd
abdmmar <opowae909@gmail.com>
2021-12-16 16:52:10 +0700

    add example

--- example.js
+++ example.js
@@ -1,15 +1,26 @@
 import { toast } from './index.js';
 
-document.querySelector('.toast').addEventListener('click', () => {
-  if ((Math.random(10) * 10) % 2 === 0) {
-    promiseToast();
-  } else {
-    manualToast();
-  }
+document.querySelector('.toast-closeable').addEventListener('click', () => {
+  toast('What!', { closeable: true });
 });
 
-function promiseToast() {
-  // Automatically add success or error toast in promise
+document.querySelector('.toast-error').addEventListener('click', () => {
+  toast.error('Authentication failed');
+});
+
+document.querySelector('.toast-success').addEventListener('click', () => {
+  toast.success('Authentication success');
+});
+
+document.querySelector('.toast-loading').addEventListener('click', () => {
+  toast.loading('Authenticating...', { duration: 4000 });
+});
+
+document.querySelector('.toast-blank').addEventListener('click', () => {
+  toast('Hello World!');
+});
+
+document.querySelector('.toast-promise').addEventListener('click', () => {
   toast.promise(
     new Promise((resolve, reject) => {
       setTimeout(() => {
@@ -26,19 +37,32 @@ function promiseToast() {
       error: 'Authentication failed!'
     }
   );
-}
+});
 
-function manualToast() {
-  // Manually dismiss toast
-  const id = toast.loading('Auth...');
-
-  setTimeout(() => {
-    if ((Math.random(10) * 10) % 2 === 0) {
-      toast.success('Auth success!');
-      toast.dismiss(id);
-    } else {
-      toast.error('Auth failed!');
-      toast.dismiss(id);
+document.querySelector('.toast-svg').addEventListener('click', () => {
+  toast('Custom SVG', {
+    icon: {
+      type: 'svg',
+      content: `<svg
+  slot="svg"
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  stroke="currentColor"
+  width="20"
+  height="20"
+>
+  <path
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+    d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+  />
+</svg>`
     }
-  }, 1000);
-}
+  });
+});
+
+document.querySelector('.toast-emoji').addEventListener('click', () => {
+  toast('Hallooo', { icon: { type: 'custom', content: '👋🏻' } });
+});
