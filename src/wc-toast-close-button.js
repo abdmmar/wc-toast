e36c3b19d419b007fc74a8c76ab2d85d6b7dbf84
abdmmar <opowae909@gmail.com>
2021-12-26 23:43:14 +0700

    docs: add JSDoc and update `custom-elements.json`

--- src/wc-toast-close-button.js
+++ src/wc-toast-close-button.js
@@ -1,3 +1,10 @@
+/**
+ * @cssprop [--wc-toast-stroke=#2a2a32] - stroke. Default is #2a2a32;
+ *
+ * @summary - button to dismiss toast item
+ *
+ * @tag wc-toast-close-button
+ */
 export default class WCToastCloseButton extends HTMLElement {
   constructor() {
     super();
--- src/wc-toast-icon.js
+++ src/wc-toast-icon.js
@@ -1,3 +1,12 @@
+/**
+ * @attr {'blank' | 'success' | 'loading' | 'error' | 'custom' | 'svg'} type - type of icon
+ *
+ * @slot svg - Put your custom svg icon here
+ *
+ * @summary - custom element to add default icon, custom emoji, or svg icon to toast items
+ *
+ * @tag wc-toast-icon
+ */
 export default class WCToastIcon extends HTMLElement {
   constructor() {
     super();
