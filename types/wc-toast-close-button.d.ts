fd1b0c5cab20b76fc0e39888e6f02c921237de6c
dubisdev <dubisdev@gmail.com>
2022-10-29 13:46:06 +0200

    chore: generate types with `yarn generate-dts`

---
+++ types/wc-toast-close-button.d.ts
@@ -0,0 +1,12 @@
+/**
+ * @cssprop [--wc-toast-stroke=#2a2a32] - stroke. Default is #2a2a32;
+ *
+ * @summary - button to dismiss toast item
+ *
+ * @tag wc-toast-close-button
+ */
+export default class WCToastCloseButton extends HTMLElement {
+    static template(): string;
+    template: HTMLTemplateElement;
+}
+//# sourceMappingURL=wc-toast-close-button.d.ts.map
